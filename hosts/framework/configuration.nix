5ef296f898c1fe280d9f664d6160555094318012
Curtis John <admin@curtisrayjohn.com>
2022-09-24 00:05:30 -0400

    add vm testing hook

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -122,6 +122,9 @@
     enableSSHSupport = true;
   };
 
+  # SSH Server
+  services.openssh.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- hosts/framework/vm-hook.nix
+++ hosts/framework/vm-hook.nix
@@ -1,3 +1,6 @@
-({options, lib, ...}: lib.mkIf (options ? virtualisation.memorySize) {
-  users.users.crutonjohn.password = "changeme";
-})
+(
+  {options, lib, ...}: 
+    lib.mkIf (options ? virtualisation.memorySize) {
+      users.users.crutonjohn.password = "changeme";
+  }
+)
