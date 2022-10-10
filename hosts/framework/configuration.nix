375f301f15ba8089b55baf6d46bf18369411b51d
Curtis John <admin@curtisrayjohn.com>
2022-10-10 13:18:03 -0400

    pin kernel version

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -15,7 +15,17 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # GRUB/Plymouth
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_19.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "Ts24tZxvbyWNicvyLu3iEOClXAnbiEmI/nBu/i/o8Ug=";
+      };
+      version = "5.19.13";
+      modDirVersion = "5.19.13";
+      };
+  });
+
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [ "nodev" ];
