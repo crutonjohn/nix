576b16e7e9665d58256171369212ce18cd6c34e7
Curtis John <admin@curtisrayjohn.com>
2023-01-23 18:30:53 -0500

    feat(laptop): add docker, wireguard scaffolding

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -14,16 +14,16 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # GRUB/Plymouth
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_19.override {
-    argsOverride = rec {
-      src = pkgs.fetchurl {
-            url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-            sha256 = "Ts24tZxvbyWNicvyLu3iEOClXAnbiEmI/nBu/i/o8Ug=";
-      };
-      version = "5.19.13";
-      modDirVersion = "5.19.13";
-      };
-  });
+#  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_19.override {
+#    argsOverride = rec {
+#      src = pkgs.fetchurl {
+#            url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+#            sha256 = "Ts24tZxvbyWNicvyLu3iEOClXAnbiEmI/nBu/i/o8Ug=";
+#      };
+#      version = "5.19.13";
+#      modDirVersion = "5.19.13";
+#      };
+#  });
 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
@@ -48,43 +48,88 @@
   networking.wireless.enable = false;
 
   networking.firewall = {
-    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
+    enable = false;
+    # allowedUDPPorts = [ 51830 ]; # Clients and peers can use the same port, see listenport
   };
   # Enable WireGuard
-  networking.wireguard.interfaces = {
+  # networking.wireguard.interfaces = {
+  #   # "wg0" is the network interface name. You can name the interface arbitrarily.
+  #   wg0 = {
+  #     # Determines the IP address and subnet of the client's end of the tunnel interface.
+  #     ips = [ "192.168.135.2/32" ];
+  #     listenPort = 51830; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+
+  #     # Path to the private key file.
+  #     #
+  #     # Note: The private key can also be included inline via the privateKey option,
+  #     # but this makes the private key world-readable; thus, using privateKeyFile is
+  #     # recommended.
+  #     privateKeyFile = "/home/crutonjohn/wireguard-keys/private";
+
+  #     peers = [
+  #       # For a client configuration, one peer entry for the server will suffice.
+
+  #       {
+  #         # Public key of the server (not a file path).
+  #         publicKey = "t8WbXbH9q5qYmER/L3b7+2HYvR6vXQbj7MsEUue3xgk=";
+
+  #         # Forward all the traffic via VPN.
+  #         allowedIPs = [ "0.0.0.0/0" "::/0" ];
+  #         # Or forward only particular subnets
+  #         #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+
+  #         # Set this to the server IP and port.
+  #         endpoint = "dyn4.crutonjohn.com:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+  #         # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+  #         persistentKeepalive = 25;
+  #       }
+  #     ];
+  #   };
+  # };
+  networking.wg-quick.interfaces = {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "192.168.135.2/24" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
+      # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
+      address = [ "192.168.135.2/32" ];
+      # The port that Wireguard listens to - recommended that this be changed from default
+      listenPort = 51830;
+      # Path to the server's private key
       privateKeyFile = "/home/crutonjohn/wireguard-keys/private";
+      dns = [ "192.168.135.1" ];
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      postUp = ''
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.135.2/32 -o wlp166s0 -j MASQUERADE
+      '';
+
+      # Undo the above
+      preDown = ''
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 192.168.135.2/32 -o wlp166s0 -j MASQUERADE
+      '';
 
       peers = [
-        # For a client configuration, one peer entry for the server will suffice.
-
-        {
-          # Public key of the server (not a file path).
+        { # peer0
           publicKey = "t8WbXbH9q5qYmER/L3b7+2HYvR6vXQbj7MsEUue3xgk=";
-
-          # Forward all the traffic via VPN.
           allowedIPs = [ "0.0.0.0/0" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-
-          # Set this to the server IP and port.
-          endpoint = "dyn4.crutonjohn.com:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
+          endpoint = "dyn4.crutonjohn.com:51820";
         }
+        # More peers can be added here.
       ];
     };
+  };
+  # Enable NAT
+  networking.nat = {
+    enable = true;
+    externalInterface = "wlp166s0";
+    internalInterfaces = [ "wg0" ];
+  };
+  # Open ports in the firewall
+  networking.firewall = {
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPorts = [ 53 51830 ];
   };
 
   # Time Zone
@@ -172,14 +217,14 @@
     isNormalUser = true;
     home = "/home/crutonjohn";
     description = "Curtis Ray John";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
   };
 
   # Sound Pipewire
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
+    enable = false;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
@@ -192,6 +237,9 @@
 
   # Podman
   virtualisation.podman.enable = true;
+
+  # Docker
+  virtualisation.docker.enable = true;
 
   # Usbmuxd
   services.usbmuxd.enable = true;
