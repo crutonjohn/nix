4e0ba2e1b4f57b29a2edba74681423157325c5a0
Curtis John <admin@curtisrayjohn.com>
2024-03-01 23:43:43 -0500

    copy from host

--- hosts/homeward/loader.nix
+++ hosts/homeward/loader.nix
@@ -1,9 +1,9 @@
 { ... }:
 
 {
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
 
   # for pinning kernel version
