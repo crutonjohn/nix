1680ad901cd4bc841c1d21210881399b0ed36a84
Curtis John <admin@curtisrayjohn.com>
2024-03-01 23:27:43 -0500

    fix bootloader

--- hosts/homeward/loader.nix
+++ hosts/homeward/loader.nix
@@ -2,11 +2,8 @@
 
 {
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiSupport = true;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.grub.useOSProber = true;
   boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
 
   # for pinning kernel version
