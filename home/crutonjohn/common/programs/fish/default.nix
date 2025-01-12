659469a20546a49ffe76eb71f252989a28c3bba4
Curtis John <admin@curtisrayjohn.com>
2025-01-12 09:37:41 -0500

    fish greeter changes

---
+++ home/crutonjohn/common/programs/fish/default.nix
@@ -0,0 +1,57 @@
+{ config, lib, pkgs, ... }: {
+
+  imports = [
+    ./greeter.nix
+  ];
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "eza";
+      k = "kubecolor";
+      kk = "kubectl";
+    };
+    functions = {
+      yy = ''
+        set tmp (mktemp -p /tmp/yazi)
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+          cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+      '';
+      ".." = ''
+        if test -z $argv[1]
+          set n 1
+        else
+          set n $argv[1]
+        end
+        for i in (seq $n)
+          cd ..
+        end      
+      '';
+    };
+    plugins = [
+      {
+        name = "fish-kubectl-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "evanlucas";
+          repo = "fish-kubectl-completions";
+          rev = "ced676392575d618d8b80b3895cdc3159be3f628";
+          sha256 = "sha256-t7FKKzD42hAZV7CpitrznRYLkCYok7Bqg1JXW7BaKyA=";
+        };
+      }
+
+      {
+        name = "ev-fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "joehillen";
+          repo = "ev-fish";
+          rev = "a3d0658c5e0563ff0f4ade9e83b20e01fea0a9e6";
+          sha256 = "sha256-t7FKKzD42hAZV7CpitrznRYLkCYok7Bqg1JXW7BaKyA=";
+        };
+      }
+    ];
+  };
+
+}
---
+++ home/crutonjohn/common/programs/fish/greeter.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }: {
+  programs.fish = {
+    functions = {
+      fish_greeting = ''
+      '';
+      fish_get_ip_addr = ''
+        ip addr | awk '/^[0-9]/ {dev=$2; up=0} /UP/ {up=1} /inet / && up {print dev, $2}' | grep -vE '(br.+|docker0|lo)' | cut -d/ -f1
+      '';
+      fish_greeter_hostname = ''
+        bash -c \
+          /usr/bin/env figlet "$(hostname)"
+      '';
+
+      fish_greeter_hostinfo = ''
+        bash -c \
+          # get load averages
+          IFS=" " read LOAD1 LOAD5 LOAD15 <<<$(cat /proc/loadavg | awk '{ print $1,$2,$3 }')
+          # get free memory
+          IFS=" " read USED AVAIL TOTAL <<<$(free -htm | grep "Mem" | awk {'print $3,$7,$2'})
+          # get processes
+          PROCESS=`ps -eo user=|sort|uniq -c | awk '{ print $2 " " $1 }'`
+          PROCESS_ALL=`echo "$PROCESS"| awk {'print $2'} | awk '{ SUM += $1} END { print SUM }'`
+          PROCESS_ROOT=`echo "$PROCESS"| grep root | awk {'print $2'}`
+          PROCESS_USER=`echo "$PROCESS"| grep -v root | awk {'print $2'} | awk '{ SUM += $1} END { print SUM }'`
+          # get processors
+          PROCESSOR_NAME=`grep "model name" /proc/cpuinfo | cut -d ' ' -f3- | awk {'print $0'} | head -1`
+          PROCESSOR_COUNT=`grep -ioP 'processor\t:' /proc/cpuinfo | wc -l`
+
+          W="\e[0;39m"
+          G="\e[1;32m"
+
+          echo -e "
+          $W  system info:
+          $W  Distro......: $W`cat /etc/*release | grep "PRETTY_NAME" | cut -d "=" -f 2- | sed 's/"//g'`
+          $W  Kernel......: $W`uname -sr`
+
+          $W  Uptime......: $W`uptime -p`
+          $W  Load........: $G$LOAD1$W (1m), $G$LOAD5$W (5m), $G$LOAD15$W (15m)
+          $W  Processes...:$W $G$PROCESS_ROOT$W (root), $G$PROCESS_USER$W (user), $G$PROCESS_ALL$W (total)
+
+          $W  CPU.........: $W$PROCESSOR_NAME ($G$PROCESSOR_COUNT$W vCPU)
+          $W  Memory......: $G$USED$W used, $G$AVAIL$W avail, $G$TOTAL$W total$W"      
+      '';
+
+    };
+  };
+
+}
