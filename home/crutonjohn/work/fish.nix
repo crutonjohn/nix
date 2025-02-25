5d8e28d4b0ae1f8c21e13f38a4a945fa3692e3ca
crutonjohn <crutonjohn@pm.me>
2025-02-25 14:07:58 -0500

    add ruby path

--- home/crutonjohn/work/fish.nix
+++ home/crutonjohn/work/fish.nix
@@ -3,5 +3,8 @@
     functions = {
       nrb = "home-manager switch --flake github:crutonjohn/nix#bjohn@work";
     };
+    shellInit = ''
+      fish_add_path $HOME/.local/share/gem/ruby/3.0.0/bin
+    '';
   };
 }
