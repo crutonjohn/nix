840bdeb3e8287c58dab82266bb24e3d7ab8035ef
Curtis John <admin@curtisrayjohn.com>
2025-07-26 11:34:13 -0400

    feat(cachix): implicitly trust cachix
    this prevents the prompt every time

--- hosts/global/meta.nix
+++ hosts/global/meta.nix
@@ -17,6 +17,13 @@
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
       ];
+      trusted-substituters = [
+        "https://cache.nixos.org/"
+        "https://cuda-maintainers.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
+      ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
