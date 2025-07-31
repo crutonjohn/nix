aa6e77320eb06fe9d1f326a10e669d4a12045984
Curtis John <admin@curtisrayjohn.com>
2025-07-31 14:32:51 -0400

    feat(bambu-studio): local installation of bambu-studio
    upstream is way out of date and not working

---
+++ overlays/bambu-studio/default.nix
@@ -0,0 +1,28 @@
+{ lib, stdenv, pkgs, appimageTools, gsettings-desktop-schemas, gtk3, fetchurl, ...}:
+let
+  version = "02.01.01.52";
+  appImage = fetchurl {
+    url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_linux_fedora-v${version}.AppImage";
+    sha256 = "sha256-PVNhyXLIjivR5dBuOWL85/c5BAL2fKx3q6/5pKReWuE=";
+  };
+in
+appimageTools.wrapType2 {
+  pname = "bambu-studio-local";
+  inherit version;
+
+  src = appImage;
+
+  profile = ''
+    export LC_ALL=C.UTF-8
+    export XDG_DATA_DIRS="${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS"
+  '';
+
+  extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs) ++ (with pkgs; [
+    # fixes "unexpected error"
+    gsettings-desktop-schemas glib gtk3
+
+    # needed for icons
+    adwaita-icon-theme
+  ]);
+}
+
