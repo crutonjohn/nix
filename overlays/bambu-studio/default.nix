953b7ac26f63cd3b0fd0c42da51a191f9f1d9893
Curtis John <crutonjohn@pm.me>
2025-10-04 00:26:12 -0400

    chore(bambu-studio): update bambu-studio

--- overlays/bambu-studio/default.nix
+++ overlays/bambu-studio/default.nix
@@ -1,9 +1,10 @@
 { lib, stdenv, pkgs, appimageTools, gsettings-desktop-schemas, gtk3, fetchurl, ...}:
 let
-  version = "02.01.01.52";
+  version = "02.02.02.56";
   appImage = fetchurl {
-    url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_linux_fedora-v${version}.AppImage";
-    sha256 = "sha256-huMKtNMW7UqsiyRuielGbF4wjUH1Jwv2tcQIBSqFqhM=";
+    #url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_linux_fedora-v${version}.AppImage";
+    url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_ubuntu-24.04_PR-8184.AppImage";
+    sha256 = "sha256:ce28a910ccf9f251bffaec6e6c2779ddce818c9f56ddda09f7f67c549a7e65ae";
   };
 in
 appimageTools.wrapType2 {
