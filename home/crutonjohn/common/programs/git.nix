866687d48fbb0ad8aa3db1aec401f328dcd081a2
Curtis John <crutonjohn@pm.me>
2025-12-03 21:58:08 -0500

    chore(git): more schema changes

--- home/crutonjohn/common/programs/git.nix
+++ home/crutonjohn/common/programs/git.nix
@@ -1,43 +1,44 @@
 { ... }: {
   programs.git = {
     enable = true;
-    settings.userEmail = "crutonjohn@pm.me";
-    settings.userName = "Curtis John";
     includes = [{ path = "~/.config/git/gitconfig-personal"; }];
-    settings.aliases = {
-      lg =
-        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit";
-      st = "status -s";
-      s = "status";
-      a = "!git add . && git status";
-      ci = "commit";
-      co = "checkout";
-      d = "difftool";
-      dp = "!git --no-pager diff";
-      ds = "diff --staged";
-      dsp = "!git --no-pager diff --staged";
-      main = "checkout main";
-      master = "checkout master";
-      no-remotes =
-        "!git branch --format '%(refname:short) %(upstream:short)' | awk '{if (!$2) print $1;}'";
-      alias =
-        "!git config --list | grep 'alias\\.' | sed 's/alias\\.\\([^=]*\\)=\\(.*\\)/\\1\\	 => \\2/' | sort";
-    };
-    settings.extraConfig = {
-      init.defaultBranch = "main";
-      merge.tool = "ediff";
-      merge.keepBackup = false;
-      merge.conflictStyle = "diff3";
-      mergetool.keepBackup = false;
-      mergetool.trustExitCode = true;
-      mergetool.ediff.keepBackup = false;
-      github.user = "crutonjohn";
-      color.ui = true;
-      fetch.prune = true;
-      pull.rebase = true;
-      push.default = "upstream";
-      push.autoSetupRemote = true;
-    };
+    settings = {
+      user.email = "crutonjohn@pm.me";
+      user.name = "Curtis John";
+      aliases = {
+        lg =
+          "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit";
+        st = "status -s";
+        s = "status";
+        a = "!git add . && git status";
+        ci = "commit";
+        co = "checkout";
+        d = "difftool";
+        dp = "!git --no-pager diff";
+        ds = "diff --staged";
+        dsp = "!git --no-pager diff --staged";
+        main = "checkout main";
+        master = "checkout master";
+        no-remotes =
+          "!git branch --format '%(refname:short) %(upstream:short)' | awk '{if (!$2) print $1;}'";
+        alias =
+          "!git config --list | grep 'alias\\.' | sed 's/alias\\.\\([^=]*\\)=\\(.*\\)/\\1\\	 => \\2/' | sort";
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        merge.tool = "ediff";
+        merge.keepBackup = false;
+        merge.conflictStyle = "diff3";
+        mergetool.keepBackup = false;
+        mergetool.trustExitCode = true;
+        mergetool.ediff.keepBackup = false;
+        github.user = "crutonjohn";
+        color.ui = true;
+        fetch.prune = true;
+        pull.rebase = true;
+        push.default = "upstream";
+        push.autoSetupRemote = true;
+      };
     ignores = [
       "#*#"
       ".#*"
