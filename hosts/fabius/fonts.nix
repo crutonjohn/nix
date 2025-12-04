08e8fba123a368f6468efc96857cf5beb6404e56
Curtis John <crutonjohn@pm.me>
2025-12-03 20:35:32 -0500

    chore(pkgs): noto-fonts-emoji to noto-fonts-color-emoji

--- hosts/fabius/fonts.nix
+++ hosts/fabius/fonts.nix
@@ -14,7 +14,7 @@
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       open-sans
       ubuntu_font_family
       iosevka
