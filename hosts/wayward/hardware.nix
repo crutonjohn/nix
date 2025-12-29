d1de85ce564c789a36d04973eb6e35c574f235f0
Curtis John <admin@curtisrayjohn.com>
2025-12-29 10:37:57 -0500

    fix(driver): package name change

--- hosts/wayward/hardware.nix
+++ hosts/wayward/hardware.nix
@@ -6,7 +6,7 @@
   hardware.enableAllFirmware = true;
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [ intel-media-driver vaapiVdpau libvdpau-va-gl ];
+    extraPackages = with pkgs; [ intel-media-driver libva-vdpau-driver libvdpau-va-gl ];
   };
   services.fwupd.enable = true;
   services.libinput.touchpad = {
