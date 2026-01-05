b777bbecd08c58d53c75bcb2b951cb55cf414a8c
Curtis John <admin@curtisrayjohn.com>
2026-01-04 19:35:54 -0500

    fix(certificates): actually read the docs this time

--- hosts/nord/certificates.nix
+++ hosts/nord/certificates.nix
@@ -1,22 +1,28 @@
 { pkgs, ... }:
 
 {
-  systemd.timers.step-cert-renew = {
-    description = "Run Step CA certificate renewal every 20 hours";
-    wantedBy = [ "timers.target" ];
+
+systemd.timers."hello-world" = {
+  wantedBy = [ "timers.target" ];
     timerConfig = {
       OnCalendar = "*-*-* 0/20:00:00"; # Runs every 20 hours
-      Persistent = true;
+      Unit = "renew-step-cert.service";
     };
-    unitConfig = {
-      ExecStart = "${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot \"/var/lib/acme/acme-challenge\" --san \"alerts.ord.heyjohn.family\" --san \"alerts.heyjohn.family\" --san \"prometheus.ord.heyjohn.family\" --san \"prometheus.heyjohn.family\" --san \"grafana.heyjohn.family\" --not-after=\"24h\" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass";
-      ExecStartPost = ''
-        chown acme:nginx /var/lib/acme/nord.heyjohn.family/cert.pem
-        chown acme:nginx /var/lib/acme/nord.heyjohn.family/key.pem
-      '';
-      User = "root";
-      Group = "root";
-    }; # Reuse the service configuration
+};
+
+systemd.services."renew-step-cert" = {
+  script = ''
+    set -eu
+    ${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot \"/var/lib/acme/acme-challenge\" --san \"alerts.ord.heyjohn.family\" --san \"alerts.heyjohn.family\" --san \"prometheus.ord.heyjohn.family\" --san \"prometheus.heyjohn.family\" --san \"grafana.heyjohn.family\" --not-after=\"24h\" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass
+    chown acme:nginx /var/lib/acme/nord.heyjohn.family/cert.pem
+    chown acme:nginx /var/lib/acme/nord.heyjohn.family/key.pem
+  '';
+  serviceConfig = {
+    Type = "oneshot";
+    User = "root";
+    Group = "root";
+    RemainAfterExit = true; # Prevents the service from automatically starting on rebuild.
   };
+};
 
 }
