b088c364ee4d7ae4548da3c54c6850559993f737
Curtis John <admin@curtisrayjohn.com>
2026-01-04 19:17:49 -0500

    fix(certificates): unit fixes

--- hosts/nord/certificates.nix
+++ hosts/nord/certificates.nix
@@ -1,26 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
-
-  stepCertRenewService = {
-    description = "Renew Step CA certificate for nord.heyjohn.family";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot \"/var/lib/acme/acme-challenge\" --san \"alerts.ord.heyjohn.family\" --san \"alerts.heyjohn.family\" --san \"prometheus.ord.heyjohn.family\" --san \"prometheus.heyjohn.family\" --san \"grafana.heyjohn.family\" --not-after=\"24h\" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass";
-      ExecStartPost = ''
-        chown acme:nginx /var/lib/acme/nord.heyjohn.family/cert.pem
-        chown acme:nginx /var/lib/acme/nord.heyjohn.family/key.pem
-      '';
-      User = "root";
-      Group = "root";
-    };
-  };
-
-in
+{ pkgs, ... }:
 
 {
-  services.step-cert-renew = stepCertRenewService;
-
   systemd.timers.step-cert-renew = {
     description = "Run Step CA certificate renewal every 20 hours";
     wantedBy = [ "timers.target" ];
@@ -28,7 +8,15 @@ in
       OnCalendar = "*-*-* 0/20:00:00"; # Runs every 20 hours
       Persistent = true;
     };
-    serviceConfig = stepCertRenewService.serviceConfig; # Reuse the service configuration
+    unitConfig = {
+      ExecStart = "${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot \"/var/lib/acme/acme-challenge\" --san \"alerts.ord.heyjohn.family\" --san \"alerts.heyjohn.family\" --san \"prometheus.ord.heyjohn.family\" --san \"prometheus.heyjohn.family\" --san \"grafana.heyjohn.family\" --not-after=\"24h\" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass";
+      ExecStartPost = ''
+        chown acme:nginx /var/lib/acme/nord.heyjohn.family/cert.pem
+        chown acme:nginx /var/lib/acme/nord.heyjohn.family/key.pem
+      '';
+      User = "root";
+      Group = "root";
+    }; # Reuse the service configuration
   };
 
 }
