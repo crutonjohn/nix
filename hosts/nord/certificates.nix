b46330c2d408091bcd318db4a5b2bb5e58c80d9e
Curtis John <admin@curtisrayjohn.com>
2026-01-04 19:55:59 -0500

    fix(certificates): file movement shenanigans cuz non-interactive tty

--- hosts/nord/certificates.nix
+++ hosts/nord/certificates.nix
@@ -13,11 +13,14 @@ systemd.timers."renew-step-cert" = {
 systemd.services."renew-step-cert" = {
   script = ''
     set -eu
-    ${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot "/var/lib/acme/acme-challenge" --san "alerts.ord.heyjohn.family" --san "alerts.heyjohn.family" --san "prometheus.ord.heyjohn.family" --san "prometheus.heyjohn.family" --san "grafana.heyjohn.family" --not-after="24h" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass
+    ${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.tmp /var/lib/acme/nord.heyjohn.family/key.tmp --webroot "/var/lib/acme/acme-challenge" --san "alerts.ord.heyjohn.family" --san "alerts.heyjohn.family" --san "prometheus.ord.heyjohn.family" --san "prometheus.heyjohn.family" --san "grafana.heyjohn.family" --not-after="24h" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass
+    mv /var/lib/acme/nord.heyjohn.family/cert.tmp /var/lib/acme/nord.heyjohn.family/cert.pem
+    mv /var/lib/acme/nord.heyjohn.family/key.tmp /var/lib/acme/nord.heyjohn.family/key.pem
     chown acme:nginx /var/lib/acme/nord.heyjohn.family/cert.pem
     chown acme:nginx /var/lib/acme/nord.heyjohn.family/key.pem
     chmod 640 /var/lib/acme/nord.heyjohn.family/cert.pem
     chmod 640 /var/lib/acme/nord.heyjohn.family/key.pem
+    rm -f /var/lib/acme/nord.heyjohn.family/*.tmp
   '';
   serviceConfig = {
     Type = "oneshot";
