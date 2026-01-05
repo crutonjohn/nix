37739a6192c63644fef292852320602c8eabf444
Curtis John <admin@curtisrayjohn.com>
2026-01-04 19:43:10 -0500

    chore(certificates): stop escaping

--- hosts/nord/certificates.nix
+++ hosts/nord/certificates.nix
@@ -13,7 +13,7 @@ systemd.timers."renew-step-cert" = {
 systemd.services."renew-step-cert" = {
   script = ''
     set -eu
-    ${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot \"/var/lib/acme/acme-challenge\" --san \"alerts.ord.heyjohn.family\" --san \"alerts.heyjohn.family\" --san \"prometheus.ord.heyjohn.family\" --san \"prometheus.heyjohn.family\" --san \"grafana.heyjohn.family\" --not-after=\"24h\" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass
+    ${pkgs.step-cli}/bin/step ca certificate nord.heyjohn.family /var/lib/acme/nord.heyjohn.family/cert.pem /var/lib/acme/nord.heyjohn.family/key.pem --webroot "/var/lib/acme/acme-challenge" --san "alerts.ord.heyjohn.family" --san "alerts.heyjohn.family" --san "prometheus.ord.heyjohn.family" --san "prometheus.heyjohn.family" --san "grafana.heyjohn.family" --not-after="24h" --provisioner registration-authority --provisioner-password-file /root/.step/registration-authority-pass
     chown acme:nginx /var/lib/acme/nord.heyjohn.family/cert.pem
     chown acme:nginx /var/lib/acme/nord.heyjohn.family/key.pem
   '';
