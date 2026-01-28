4da00d58f91d11f3d09c704b0e0d870146ec7ec1
Curtis John <admin@curtisrayjohn.com>
2026-01-27 21:53:52 -0500

    feat(anytype): add desktop entry

--- home/crutonjohn/wayward/desktop.nix
+++ home/crutonjohn/wayward/desktop.nix
@@ -1,27 +1,38 @@
-{ ... }: {
+{ ... }:
+{
 
-xdg.desktopEntries = {
-  lychee-slicer = {
-    name = "Lychee Slicer";
-    genericName = "3D Printing Slicer";
-    exec = "lychee-slicer %U";
-    terminal = false;
-    categories = [ "Application" "Printing" ];
+  xdg.desktopEntries = {
+    lychee-slicer = {
+      name = "Lychee Slicer";
+      genericName = "3D Printing Slicer";
+      exec = "lychee-slicer %U";
+      terminal = false;
+      categories = [
+        "Application"
+        "Printing"
+      ];
+    };
+    pog = {
+      name = "Pog Configurator";
+      genericName = "KMK Keyboard Configuration GUI";
+      exec = "pog %U";
+      terminal = false;
+      categories = [ "Utility" ];
+    };
+    newrecruit-builder = {
+      name = "New Recruit - Builder";
+      genericName = "Builder";
+      exec = "newrecruit-builder %U";
+      terminal = false;
+      categories = [ "Utility" ];
+    };
+    anytype = {
+      name = "Anytype";
+      genericName = "Anytype";
+      exec = "anytype %U";
+      terminal = false;
+      categories = [ "Application" ];
+    };
   };
-  pog = {
-    name = "Pog Configurator";
-    genericName = "KMK Keyboard Configuration GUI";
-    exec = "pog %U";
-    terminal = false;
-    categories = [ "Utility" ];
-  };
-  newrecruit-builder = {
-    name = "New Recruit - Builder";
-    genericName = "Builder";
-    exec = "newrecruit-builder %U";
-    terminal = false;
-    categories = [ "Utility" ];
-  };
-};
 
 }
