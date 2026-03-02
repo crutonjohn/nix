5ba19419912e112e18d2cb98274d36ae7929834c
Curtis John <admin@curtisrayjohn.com>
2026-03-02 17:42:46 -0500

    feat(kanshi): add monitor

--- home/kat/cawl/kanshi.nix
+++ home/kat/cawl/kanshi.nix
@@ -24,13 +24,22 @@
     ".config/kanshi/config".text = ''
       output "Samsung Electric Company Odyssey G95C HNTX401810" {
         mode 5120x1440@119.97
-        position 0,0
+        position 2160,1260
         scale 1
         alias $G9_1
       }
 
+      output "LG Electronics LG ULTRAGEAR 404NTTQD1705" {
+        mode 3840x2160@143.999
+        position 0,0
+        scale 1
+        alias $LG_1
+        transform 90
+      }
+
       profile HOME_1 {
         output $G9_1 enable
+        output $LG_1 enable
       }
     '';
   };
