080ef736cebb917c500533ee757eaff23557d7f5
Curtis John <admin@curtisrayjohn.com>
2026-03-02 16:02:26 -0500

    fix(cawl): networking fixes

--- hosts/cawl/networking.nix
+++ hosts/cawl/networking.nix
@@ -1,18 +1,21 @@
 { ... }:
 
 {
-  networking.hostName = "fabius";
+  networking.hostName = "cawl";
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = false;
     allowedTCPPorts = [ 53 ];
-    allowedUDPPorts = [ 53 51830 ];
+    allowedUDPPorts = [
+      53
+      51830
+    ];
     # allowedUDPPorts = [ 51830 ]; # Clients and peers can use the same port, see listenport
   };
   networking.nat = {
     enable = true;
-    externalInterface = "enp8s0";
-#    internalInterfaces = [ "wg0" ];
+    externalInterface = "enp15s0";
+    #    internalInterfaces = [ "wg0" ];
   };
 
   # tailscale daemon
