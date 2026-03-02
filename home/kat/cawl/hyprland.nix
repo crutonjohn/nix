83c2be3caf0923b2a284043e7247f600a0731aff
Curtis John <admin@curtisrayjohn.com>
2026-03-02 16:26:46 -0500

    feat(cawl): remove crutonjohn refs

--- home/kat/cawl/hyprland.nix
+++ home/kat/cawl/hyprland.nix
@@ -1,9 +1,9 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }:
+{
 
   wayland.windowManager.hyprland = {
     enable = true;
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # needed due to https://github.com/hyprwm/Hyprland/discussions/4768
     extraConfig = ''
       device {
@@ -148,10 +148,8 @@
         "SUPER,Tab,cyclenext,"
 
         # Gaps
-        ''
-          SUPER SHIFT,G,exec,hyprctl --batch "keyword general:gaps_out 5;keyword general:gaps_in 3"''
-        ''
-          SUPER,G,exec,hyprctl --batch "keyword general:gaps_out 0;keyword general:gaps_in 0"''
+        ''SUPER SHIFT,G,exec,hyprctl --batch "keyword general:gaps_out 5;keyword general:gaps_in 3"''
+        ''SUPER,G,exec,hyprctl --batch "keyword general:gaps_out 0;keyword general:gaps_in 0"''
 
         # Focus movement
         "SUPER,left,movefocus,l"
@@ -276,7 +274,10 @@
       #  reset = {};
       #};
 
-      bindm = [ "SUPER,mouse:272,movewindow" "SUPER,mouse:273,resizewindow" ];
+      bindm = [
+        "SUPER,mouse:272,movewindow"
+        "SUPER,mouse:273,resizewindow"
+      ];
 
       exec = [ "kanshictl reload" ];
 
@@ -286,10 +287,8 @@
         "waybar --config ~/.config/waybar/config &"
         "nm-applet --indicator &"
         # auto toggle waybar on gaming workspace
-        "/etc/profiles/per-user/crutonjohn/bin/hyprland-gaming-auto-waybar-toggle &>/dev/null"
+        "/etc/profiles/per-user/kat/bin/hyprland-gaming-auto-waybar-toggle &>/dev/null"
         # autostart programs #
-        "/etc/profiles/per-user/crutonjohn/bin/discord &"
-        "/etc/profiles/per-user/crutonjohn/bin/floorp &"
         "hyprland-gaming-init"
         ######################
       ];
@@ -417,25 +416,29 @@
         };
       };
 
-      background = [{
-        path = "~/.config/docked.jpg";
-        blur_passes = 3;
-        blur_size = 8;
-      }];
+      background = [
+        {
+          path = "~/.config/docked.jpg";
+          blur_passes = 3;
+          blur_size = 8;
+        }
+      ];
 
-      input-field = [{
-        size = "200, 50";
-        position = "0, -80";
-        monitor = "";
-        dots_center = true;
-        fade_on_empty = false;
-        font_color = "rgb(202, 211, 245)";
-        inner_color = "rgb(91, 96, 120)";
-        outer_color = "rgb(24, 25, 38)";
-        outline_thickness = 5;
-        placeholder_text = "<span foreground='##cad3f5'>Password...</span>";
-        shadow_passes = 2;
-      }];
+      input-field = [
+        {
+          size = "200, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          outline_thickness = 5;
+          placeholder_text = "<span foreground='##cad3f5'>Password...</span>";
+          shadow_passes = 2;
+        }
+      ];
     };
   };
 
@@ -457,8 +460,7 @@
     '';
   };
 
-  systemd.user.targets.hyprland-session.Unit.Wants =
-    [ "xdg-desktop-autostart.target" ];
+  systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
 
   ####################
   # Hyprland Scripts #
