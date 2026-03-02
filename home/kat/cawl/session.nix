5e7dc965da6cf9e24cab9878fbdadfc36fff8a76
Curtis John <admin@curtisrayjohn.com>
2026-03-02 17:05:36 -0500

    fix(hyprland): add card by path

--- home/kat/cawl/session.nix
+++ home/kat/cawl/session.nix
@@ -3,9 +3,10 @@
 {
   home = {
     sessionVariables = {
-      AQ_DRM_DEVICES = "/dev/dri/card0";
+      AQ_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card";
+      WLR_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card";
       NIXOS_OZONE_WL = "1";
-      WLR_DRM_DEVICES = "/dev/dri/card0";
+
     };
   };
 }
