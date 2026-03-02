f05f07d9482fc400d772fec21aa98588dba45910
Curtis John <admin@curtisrayjohn.com>
2026-03-02 17:22:45 -0500

    fix

--- home/crutonjohn/cawl/session.nix
+++ home/crutonjohn/cawl/session.nix
@@ -5,8 +5,8 @@
     sessionVariables = {
       # AQ_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card";
       # WLR_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card";
-      LIBVA_DRIVER_NAME = nvidia;
-      __GLX_VENDOR_LIBRARY_NAME = nvidia;
+      LIBVA_DRIVER_NAME = "nvidia";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       NIXOS_OZONE_WL = "1";
     };
   };
