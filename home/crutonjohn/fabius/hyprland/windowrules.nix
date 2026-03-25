067a75a8964b379fe747781b29549129203c207a
Curtis John <crutonjohn@pm.me>
2026-03-25 19:06:08 -0400

    feat(fabius): tinkering with game window rules

--- home/crutonjohn/fabius/hyprland/windowrules.nix
+++ home/crutonjohn/fabius/hyprland/windowrules.nix
@@ -12,7 +12,15 @@
         "tag +game, initialClass:^(cs2|steam_app_.*|gamescope)$"
         "tag +game, initialTitle:^(Arc Raiders)$"
         "noborder, tag:game"
-
+        "no_blur on, tag:game"
+        "no_dim on, tag:game"
+        "no_shadow on, tag:game"
+        "opaque on, tag:game"
+        "no_anim on, tag:game"
+        "rounding 0, tag:game"
+        "fullscreen on, tag:game"
+        "monitor DP-2, tag:game"
+        "workspace 1, tag:game"
       ];
     };
   };
