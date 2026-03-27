e1e1792ed64f9438e506c0e8e38518167e9b1ee5
Curtis John <admin@curtisrayjohn.com>
2026-03-27 18:50:21 -0400

    feat(fabius): new window rule for gaming

--- home/crutonjohn/fabius/hyprland/windowrules.nix
+++ home/crutonjohn/fabius/hyprland/windowrules.nix
@@ -12,15 +12,7 @@
         "tag +game, initialClass:^(cs2|steam_app_.*|gamescope)$"
         "tag +game, initialTitle:^(Arc Raiders)$"
         "noborder, tag:game"
-        "no_blur on, initialClass:^(cs2|steam_app_.*|gamescope)$"
-        "no_dim on, initialClass:^(cs2|steam_app_.*|gamescope)$"
-        "no_shadow on, initialClass:^(cs2|steam_app_.*|gamescope)$"
-        "opaque on, tag:game"
-        "no_anim on, initialClass:^(cs2|steam_app_.*|gamescope)$"
-        "rounding 0, tag:game"
-        "fullscreen on, initialClass:^(cs2|steam_app_.*|gamescope)$"
-        "monitor DP-2, tag:game"
-        "workspace 1, tag:game"
+        "noblur on, nodim on, noshadow on, opaque on, noanim on, rounding 0, fullscreen, monitor DP-2, workspace 1, tag:game"
       ];
     };
   };
