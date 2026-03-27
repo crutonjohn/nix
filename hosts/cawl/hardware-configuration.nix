50572b575e89755bee733147c4e4c423650c9b6e
Curtis John <admin@curtisrayjohn.com>
2026-03-27 19:50:39 -0400

    revert(cawl): filesystem permissions

--- hosts/cawl/hardware-configuration.nix
+++ hosts/cawl/hardware-configuration.nix
@@ -36,8 +36,9 @@
     device = "/dev/disk/by-uuid/5fff62be-4084-4cde-b810-c7c58519d65f";
     fsType = "ext4";
     options = [
-      "uid=kat"
-      "gid=users"
+      # "uid=kat"
+      # "gid=users"
+      # "mode=775"
     ];
   };
 
