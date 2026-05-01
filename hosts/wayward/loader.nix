cf9d158ae16a0f6546506f7e62e1a699f927c01d
Curtis John <admin@curtisrayjohn.com>
2026-05-01 18:23:05 -0400

    feat(wayward): copyfail protection

--- hosts/wayward/loader.nix
+++ hosts/wayward/loader.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   boot.loader.efi.canTouchEfiVariables = true;
@@ -7,7 +7,15 @@
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.grub.useOSProber = true;
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "nvidia"
+  ];
+
+  # CopyFail
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.18.22") (
+    lib.mkDefault pkgs.linuxPackages_6_18
+  );
 
   # for pinning kernel version
   #  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_19.override {
