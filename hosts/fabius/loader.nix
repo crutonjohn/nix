6fbd805db29bb2af74febe612d970e5a8f803ebf
Curtis John <admin@curtisrayjohn.com>
2026-05-01 20:41:33 -0400

    feat(fabius): copyfail

--- hosts/fabius/loader.nix
+++ hosts/fabius/loader.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   boot.loader.efi.canTouchEfiVariables = true;
@@ -6,15 +6,23 @@
     enable = true;
     # used to find UEFI bootable Windows Parts
     #edk2-uefi-shell.enable = true;
-    windows = {
-      "11" = {
-        title = "Windows 11 Shitty OS Edition";
-        efiDeviceHandle = "HD2b";
-      };
-    };
+    # windows = {
+    #   "11" = {
+    #     title = "Windows 11 Shitty OS Edition";
+    #     efiDeviceHandle = "HD2b";
+    #   };
+    # };
   };
   boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "nvidia"
+  ];
+
+  # CopyFail
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.18.22") (
+    lib.mkDefault pkgs.linuxPackages_6_18
+  );
 
   # for pinning kernel version
   #  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_19.override {
