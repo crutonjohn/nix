43af2d659cd6e1dcdaaa2924b2fd98cb7875e996
Curtis John <crutonjohn@pm.me>
2026-05-26 21:05:19 -0400

    chore(cawl): revert monitor settings

--- home/kat/cawl/hyprland/monitor.nix
+++ home/kat/cawl/hyprland/monitor.nix
@@ -3,10 +3,12 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-2, 1920x1080@144.00, 0x0, 1"
-        # "DP-2, 3840x2160@144.00, 0x0, 1.5, transform, 1"
-        # "DP-1, 5120x1440@119.99900, 1440x790, 1"
-        # "DP-1, addreserved, 0, 0, 1280, 1280"
+        # For LAN 1080p Monitor
+        # "DP-2, 1920x1080@144.00, 0x0, 1"
+        # For at-home usage
+        "DP-2, 3840x2160@144.00, 0x0, 1.5, transform, 1"
+        "DP-1, 5120x1440@119.99900, 1440x790, 1"
+        "DP-1, addreserved, 0, 0, 1280, 1280"
       ];
     };
   };
