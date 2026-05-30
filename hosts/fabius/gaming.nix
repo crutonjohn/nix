c78ac06db0eecece945102209537ba103abe7793
Curtis John <crutonjohn@pm.me>
2026-05-30 14:29:00 -0400

    feat(fabius): add sunshine

--- hosts/fabius/gaming.nix
+++ hosts/fabius/gaming.nix
@@ -62,4 +62,24 @@
     sdl3-image
   ];
 
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    openFirewall = true;
+    capSysAdmin = true;
+    # apps = [
+    #   {
+    #     name = "1440p Desktop";
+    #     prep-cmd = [
+    #       {
+    #         do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.2560x1440@144";
+    #         undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.3440x1440@144";
+    #       }
+    #     ];
+    #     exclude-global-prep-cmd = "false";
+    #     auto-detach = "true";
+    #   }
+    # ];
+  };
+
 }
